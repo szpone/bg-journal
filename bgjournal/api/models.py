2d999a2eb5ecb867e03d42ec1238d49c3bab112a
szpone <nikola.adamus@gmail.com>
2019-01-15 18:55:00 +0100

    add urls

--- bgjournal/api/models.py
+++ bgjournal/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BoardGame(models.Model):
+    name = models.CharField(max_length=128)
+    edition = models.IntegerField(null=True, blank=True)
+
+
+class Expansion(models.Model):
+    name = models.CharField(max_length=128)
+    board_game = models.ForeignKey(BoardGame, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Match(models.Model):
+    victory = models.BooleanField(default=False)
+    points = models.IntegerField(default=0, null=True, blank=True)
+    scenario = models.CharField(max_length=128, null=True, blank=True)
+    board_game = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
+    players = models.ManyToManyField('auth.User', related_name='players')
+
+
+
+
--- bgjournal/bgjournal/settings.py
+++ bgjournal/bgjournal/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api',
-    'api',
-    'djangorestframework'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [

6df24cf2b19b8e366eb48c5c560ca8ad89ab635c
Piotr Sokołowski <piotr@sokolowscy.net>
2019-01-20 00:12:34 +0100

    FIX: Fix referencing db container + compose file
    Database 'db' container is earchable via 'db' hostname so we can use it
    to point to proper database instance when running via docker-compose
    
    Fix bgjournal container by using name for it instead of building through
    docker-compose

--- bgjournal/bgjournal/settings.py
+++ bgjournal/bgjournal/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bgjournal',
         'USER': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }

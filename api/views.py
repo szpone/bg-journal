efcc44bc5b243d8cd9cb86452a5305c4d594464e
szpone <nikola.adamus@gmail.com>
2019-03-18 19:57:02 +0100

    ADD: add ipython, datefield and top three endpoint

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
-from rest_framework.generics import ListAPIView, ListCreateAPIView, APIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView
 from django.contrib.auth import get_user_model
 from .models import BoardGame, Match, Expansion
 from .serializers import (UserSerializer, BoardGameSerializer, MatchSerializer, ExpansionSerializer,
-                          DashboardSerializer)
+                          )
+from django.db.models import Count
 
 User = get_user_model()
 
@@ -30,6 +31,16 @@ class MatchListView(ListCreateAPIView):
 class ExpansionListView(ListAPIView):
     queryset = Expansion.objects.all()
     serializer_class = ExpansionSerializer
+
+
+class TopThreeListView(ListAPIView):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
+
+    def list(self, request):
+        qs = self.get_queryset()
+        user = request.user
+        return qs.filter(players=user).values('board_game').annotate(ct=Count('board_game')).order_by('-ct')[:3]
 
 
 
